b897ff98190fe849bd55d2891b8c04c050624ce7
Prashant Bajgain <200513649@student.georgianc.on.ca>
2023-06-08 11:19:09 -0400

    lab 2 done

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,18 +1,33 @@
-X#!/bin/bash
+#!/bin/bash
+
+echo "Report for pc 200513649"
+
 
 # Display the fully-qualified domain name (FQDN)
-fqdn=$(hostname)
+fqdn=$(hostname -f)
 echo "Fully-Qualified Domain Name (FQDN): $fqdn"
 
 # Display the operating system name and version
-os_info=$(hostnamectl | grep "Operating System:")
+os_info=$(hostnamectl | awk -F: '/Operating System:/ {print $2}')
 echo "Operating System: $os_info"
 
 # Display IP addresses (excluding those starting with 127)
-ip_addresses=$(hostname -I | awk '{for(i=1;i<=NF;i++)if($i!~/^127\./)print $i}')
+ip_addresses=$(hostname -I | awk '$1 !~ /^127/ {print $1}')
 echo "IP Addresses: $ip_addresses"
 
 # Display the available space in the root filesystem
 root_space=$(df -h --output=avail / | awk 'NR==2')
 echo "Root Filesystem Space Available: $root_space"
 
+# Generate the report
+report=$(printf "$output_template" \
+    "System Information" \
+    "------------------" \
+    "Hostname:" "$hostname" \
+    "Fully Qualified Domain Name:" "$fqdn" \
+    "Operating System:" "$os_name $os_version" \
+    "IP Address:" "$ip_address" \
+    "Root Filesystem Space:" "$disk_space")
+
+# Display the report
+echo "$report"
