c3c8f5694822523bd8b5fc972f56120809efd42d
Prashant Bajgain <200513649@student.georgianc.on.ca>
2023-06-30 19:44:23 -0400

    sysinfo.sh

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -31,3 +31,75 @@ report=$(printf "$output_template" \
 
 # Display the report
 echo "$report"
+
+
+# System Information Report
+
+# Check if the user has root privilege
+if [[ $EUID -ne 0 ]]; then
+  echo "This script requires root privilege. Please run it as root or with sudo."
+  exit 1
+fi
+
+###############
+# Functions   #
+###############
+
+# Function to get system description
+get_system_description() {
+  manufacturer=$(dmidecode -s system-manufacturer)
+  model=$(dmidecode -s system-product-name)
+  serial=$(dmidecode -s system-serial-number)
+
+  echo "System Description:"
+  echo "  Manufacturer: $manufacturer"
+  echo "  Model: $model"
+  echo "  Serial Number: $serial"
+  echo
+}
+
+# Function to get CPU information
+get_cpu_info() {
+  cpu=$(lshw -class processor | awk '/product/ {print $2}')
+  architecture=$(lshw -class processor | awk '/width/ {print $2}')
+  cores=$(lscpu | awk '/^CPU\(s\):/ {print $2}')
+  max_speed=$(lscpu | awk '/^CPU MHz:/ {printf "%.2f GHz\n", $3/1000}')
+  caches=$(lscpu | awk '/^L[1-3] cache:/ {printf "%.2f MB\n", $3/1024}')
+
+  echo "CPU Information:"
+  echo "  CPU: $cpu"
+  echo "  Architecture: $architecture"
+  echo "  Core Count: $cores"
+  echo "  Maximum Speed: $max_speed"
+  echo "  Cache Sizes:"
+  echo "    L1: $(lscpu | awk '/^L1d cache:/ {printf "%.2f MB\n", $3/1024}')"
+  echo "    L2: $(lscpu | awk '/^L2 cache:/ {printf "%.2f MB\n", $3/1024}')"
+  echo "    L3: $caches"
+  echo
+}
+
+# Function to get operating system information
+get_os_info() {
+  distro=$(lsb_release -ds)
+  version=$(lsb_release -rs)
+
+  echo "Operating System Information:"
+  echo "  Linux Distribution: $distro"
+  echo "  Distro Version: $version"
+  echo
+}
+
+###############
+# Main        #
+###############
+
+# Execute functions and generate the system report
+
+# Section: System Description
+get_system_description
+
+# Section: CPU Information
+get_cpu_info
+
+# Section: Operating System Information
+get_os_info
